cef8fa7f0e2bb7b947ff2c680be9278b5cf2b963
tgaff <tgaff@alumni.nd.edu>
2016-04-17 12:00:35 -0700

    switch to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160417181122) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "goats", force: :cascade do |t|
     t.string   "name"
     t.integer  "charisma"

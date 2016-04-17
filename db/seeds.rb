8196db1577177eca293e114ee0b1abc6689be527
tgaff <tgaff@alumni.nd.edu>
2016-04-17 11:10:52 -0700

    seed some goats

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+
+
+goat_list = [
+  {name: 'Jorge',
+   charisma: 3,
+   latitude: 30.703639,
+   longitude: -9.861236,
+   color: 'grey',
+   birthdate: 300.days.ago
+  },
+  {name: 'Maude',
+   charisma: 4,
+   latitude: 30.703641,
+   longitude: -9.861238,
+   color: 'mauve',
+   birthdate: 200.days.ago
+  },
+  {name: 'Methuselah',
+   charisma: 4,
+   latitude: 30.69585,
+   longitude: -9.871111,
+   color: 'grey',
+   birthdate: 3000.days.ago
+  },
+  {name: 'Claudia',
+   charisma: 8,
+   latitude: 30.695,
+   longitude: -9.87,
+   color: 'grey',
+   birthdate: 30.days.ago
+  },
+]
+
+goat_list.each do |goat|
+  Goat.create goat
+end

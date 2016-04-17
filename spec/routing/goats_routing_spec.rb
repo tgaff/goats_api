446229a7bd0e30f1353109153c40abc7372960fc
tgaff <tgaff@alumni.nd.edu>
2016-04-17 10:57:11 -0700

    move goats to /api/goats

--- spec/routing/goats_routing_spec.rb
+++ spec/routing/goats_routing_spec.rb
@@ -4,27 +4,27 @@ RSpec.describe GoatsController, type: :routing do
   describe "routing" do
 
     it "routes to #index" do
-      expect(:get => "/goats").to route_to("goats#index")
+      expect(:get => "/api/goats").to route_to("goats#index")
     end
 
     it "routes to #show" do
-      expect(:get => "/goats/1").to route_to("goats#show", :id => "1")
+      expect(:get => "/api/goats/1").to route_to("goats#show", :id => "1")
     end
 
     it "routes to #create" do
-      expect(:post => "/goats").to route_to("goats#create")
+      expect(:post => "/api/goats").to route_to("goats#create")
     end
 
     it "routes to #update via PUT" do
-      expect(:put => "/goats/1").to route_to("goats#update", :id => "1")
+      expect(:put => "/api/goats/1").to route_to("goats#update", :id => "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(:patch => "/goats/1").to route_to("goats#update", :id => "1")
+      expect(:patch => "/api/goats/1").to route_to("goats#update", :id => "1")
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/goats/1").to route_to("goats#destroy", :id => "1")
+      expect(:delete => "/api/goats/1").to route_to("goats#destroy", :id => "1")
     end
 
   end

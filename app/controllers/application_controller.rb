4e9447406a7f88c9d99369237231d2f390b51474
tgaff <tgaff@alumni.nd.edu>
2016-04-17 12:40:51 -0700

    set default to json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  # switch all defaults to not html (why isn't this part of rails-api ?)
+  before_action :set_default_response_format
+
+  protected
+
+  def set_default_response_format
+    request.format = :json
+  end
+
+
 end
